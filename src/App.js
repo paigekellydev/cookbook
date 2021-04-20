d17fa8f7be026ea665cee45c5052e5703b7b072b
Paige Kelly <pckelly.co@gmail.com>
2021-04-20 11:12:02 -0600

    added README.md and category buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section id="categories-section" />
+        <button id="appetizers">Appetizers</button>
+        <button id="breads">Breads</button>
+        <button id="desserts">Desserts</button>
+        <button id="meats">Meats</button>
+        <button id="pasta">Pasta</button>
+        <button id="poultry">Poultry</button>
+        <button id="salad-soups">Salads and Soups</button>
+        <button id="vegetables">Vegetables</button>
     </div>
   );
 }
